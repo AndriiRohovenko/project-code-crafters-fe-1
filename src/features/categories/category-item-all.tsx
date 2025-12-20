2a93e5080b7b38ac129921b70155bd3efcbbb558
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-12-20 20:16:30 +0200

    add click event on categories section

--- src/features/categories/category-item-all.tsx
+++ src/features/categories/category-item-all.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 export const CategoryItemAll = () => {
+  const navigate = useNavigate();
+
+  const handleAllCategoriesClick = () => {
+    navigate('/?category=all');
+  };
+
   return (
     <div className="flex h-[250px] w-full flex-col rounded-[30px] bg-[#050505] bg-cover bg-center bg-no-repeat p-6 md:h-[369px]">
-      <a className="flex size-full items-center justify-center" href="#">
+      <button
+        className="flex size-full items-center justify-center transition-opacity hover:opacity-80"
+        onClick={handleAllCategoriesClick}
+      >
         <span className="text-xl font-extrabold text-white">
           ALL CATEGORIES
         </span>
-      </a>
+      </button>
     </div>
   );
 };
