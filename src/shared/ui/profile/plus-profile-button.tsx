c028c7b7af25108dec7283040c40c4df9ef821de
Andriy <andygrigs88@gmail.com>
2025-12-20 19:50:58 +0100

    fix plus icon

--- src/shared/ui/profile/plus-profile-button.tsx
+++ src/shared/ui/profile/plus-profile-button.tsx
@@ -12,7 +12,7 @@ const PlusProfileButton = () => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M10 4V16M4 10H16"
+          d="M8 2V14M2 8H14"
           stroke="currentColor"
           strokeWidth="2"
           strokeLinecap="round"
