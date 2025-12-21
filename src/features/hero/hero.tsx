dc14af9bc1d5a8e9d49186b79a48701945f81efd
Andrii Slobodianiuk <speshmillo@gmail.com>
2025-12-21 13:59:28 +0100

    .

--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -4,10 +4,10 @@ import { OutlineNavButton } from '@/shared/ui/outlinedNavButton.tsx';
 
 export const Hero = () => {
   return (
-    <section className="position-relative z-[1] mt-[-60px] pt-1 xs:pt-2 md:mt-[-90px] md:pt-4 2xl:pt-5">
-      <div className="mx-auto w-full px-1 xs:w-[100%] xs:px-2 md:px-4 2xl:w-[1440px] 2xl:px-5">
-        <div className="max-2xl:pt-[217px] max-2xl:pb-[94px] max-md:pt-[194px] max-md:pb-[100px] relative flex flex-col items-center overflow-hidden rounded-2xl bg-black pb-[81px] pt-[140px] text-center text-white md:pt-[147px]">
-          <h1 className="w-[100%] text-center text-[40px] font-extrabold uppercase leading-none xs:min-w-[304px] xs:max-w-[400px] md:max-w-[800px] md:text-[70px] 2xl:w-[80%] 2xl:max-w-[900px] 2xl:text-[90px]">
+    <section className="position-relative xs:pt-2 z-[1] mt-[-60px] pt-1 md:mt-[-90px] md:pt-4 2xl:pt-5">
+      <div className="xs:w-[100%] xs:px-2 mx-auto w-full px-1 md:px-4 2xl:w-[1440px] 2xl:px-5">
+        <div className="relative flex flex-col items-center overflow-hidden rounded-2xl bg-black pb-[81px] pt-[140px] text-center text-white max-2xl:pb-[94px] max-2xl:pt-[217px] max-md:pb-[100px] max-md:pt-[194px] md:pt-[147px]">
+          <h1 className="xs:min-w-[304px] xs:max-w-[400px] w-[100%] text-center text-[40px] font-extrabold uppercase leading-none md:max-w-[800px] md:text-[70px] 2xl:w-[80%] 2xl:max-w-[900px] 2xl:text-[90px]">
             <span className="xs-h inline-block whitespace-pre-line md:h-[160px] 2xl:h-[180px]">
               <Typewriter
                 onInit={(typewriter) => {
@@ -34,7 +34,7 @@ export const Hero = () => {
             </span>
           </h1>
 
-          <p className="max-md:mb-5 max-md:max-w-full max-md:px-2 max-md:text-sm max-md:leading-5 mx-auto mb-8 mt-[40px] max-w-[480px] px-0 pl-[12px] pr-[12px] text-base font-medium leading-6 text-white 2xl:max-w-[577px]">
+          <p className="mx-auto mb-8 mt-[40px] max-w-[480px] px-0 pl-[12px] pr-[12px] text-base font-medium leading-6 text-white max-md:mb-5 max-md:max-w-full max-md:px-2 max-md:text-sm max-md:leading-5 2xl:max-w-[577px]">
             Amazing recipes for beginners in the world of cooking, enveloping
             you in the aromas and tastes of various cuisines.
           </p>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,29 +15,7 @@ const Home = () => {
     <div>
       <Hero />
 
-      <section className="xl:pt-[120px] pt-16 md:pt-[100px]">
-        {/* <div className="mx-auto xs:w-[343px] md:w-[704px] 2xl:w-[846px]">
-
-      <section className="xl:pt-[120px] pt-16 md:pt-[100px]">
-        <div className="mx-auto xs:w-[343px] md:w-[704px] 2xl:w-[846px]">
->>>>>>> origin/main
-          <h2>followers</h2>
-          <ProfileFollowPanel tab="followers" />
-        </div>
-        <div className="mx-auto xs:w-[343px] md:w-[704px] 2xl:w-[846px]">
-          <h2>following</h2>
-          <ProfileFollowPanel tab="following" />
-        </div> */}
-      </section>
       {categoryParam ? (
-        // <section className="xl:pt-[120px] pt-16 md:pt-[100px]">
-        //   <div className="mx-auto max-w-[1280px] px-4 md:px-8 2xl:px-0">
-        //     <h2 className="mb-4 text-2xl font-bold">Recipes Filter Section</h2>
-        //     <p className="text-lg">
-        //       Showing recipes for category: {categoryParam}
-        //     </p>
-        //   </div>
-        // </section>
         <CategoryDetails categoryName={categoryParam} />
       ) : (
         <Categories />
